b17e74174ff1e2afba8d49150b217db7729afcc1
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-07-18 13:45:46 +0100

    add presence  validation to all columns in buddies table

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -1,7 +1,7 @@
 class Buddy < ApplicationRecord
   has_one_attached :photo
   belongs_to :owner, class_name: "User"
-  validates :name, :age, :description, presence: true
+  validates :name, :age, :description, :photo, :category, :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
