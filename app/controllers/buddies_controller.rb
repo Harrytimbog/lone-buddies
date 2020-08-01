127d83c3a1dc1620145dcf96d3898b9121a0a579
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-08-01 13:09:37 +0100

    make buddy index and show accessible before log in

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,4 +1,5 @@
 class BuddiesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_buddy, only: [:show, :edit, :update, :destroy]
 
   def index
